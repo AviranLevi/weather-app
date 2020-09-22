c4ec183b1c83e5e28f6d7459920eb31acb9f7455
Aviran Levi <aveloofficial@gmail.com>
2020-09-22 15:44:34 +0300

    Updates

---
+++ precache-manifest.66b3ba15e48d97b3dabe2497658fa002.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbbf10e116ba69c84fe14f587181e049",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "68df265f3f33ec4c73bb",
+    "url": "/weather-app/static/css/main.e1f3d830.chunk.css"
+  },
+  {
+    "revision": "bef5c2a4d9f2dbe98acb",
+    "url": "/weather-app/static/js/2.7f111716.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/weather-app/static/js/2.7f111716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68df265f3f33ec4c73bb",
+    "url": "/weather-app/static/js/main.7d36b579.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/weather-app/static/media/15-s.39ac499c.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/weather-app/static/media/16-s.0cf9cad1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/weather-app/static/media/17-s.3c85da3e.png"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/weather-app/static/media/41-s.421c13f1.png"
+  }
+]);
